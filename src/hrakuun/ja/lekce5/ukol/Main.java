cb85ee8f497a1259f9db5bb490bca53294783bf5
Jan Maxa <hrakuun@gmail.com>
2024-02-26 20:00:42 +0100

    # loaded list from file
    - test printout to check if data loaded sucesfuly

--- src/hrakuun/ja/lekce5/ukol/Main.java
+++ src/hrakuun/ja/lekce5/ukol/Main.java
@@ -4,6 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+        ListOfPlants listOfPlants1 = new ListOfPlants();
+        try {
+            listOfPlants1.loadPlantsFromFile(Settings.getFilename());
+        } catch (PlantException e) {
+            System.err.println("Nepodařilo se načíst obsah košíku ze souboru:" +Settings.getFilename()+":\n"+e.getLocalizedMessage());
+        }
+
+        for (Plant plant : listOfPlants1.getPlants()) {
+            System.out.println(plant);
+        }
+
     }
 
 }
