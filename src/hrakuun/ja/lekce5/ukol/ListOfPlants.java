d832a9c30994d9f35ec2bda11f8384a9a9543217
Jan Maxa <hrakuun@gmail.com>
2024-02-26 14:22:27 +0100

    # added methods addPlant, getPlantOnIndex, removePlant
    - addPlant adds given plant to the list
    - getPlantOnIndex returns plant on index from the list
    - removePlant removes given plant from the list

--- src/hrakuun/ja/lekce5/ukol/ListOfPlants.java
+++ src/hrakuun/ja/lekce5/ukol/ListOfPlants.java
@@ -4,7 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListOfPlants {
+    // region variables
+    private List<Plant> plants = new ArrayList<>();
+//    endregion
 
-    List<Plant> plants = new ArrayList<>();
+//    region methods
 
+    public void addPlant(Plant plant) {
+        plants.add(plant);
+    }
+
+    public Plant getPlantOnIndex(int index) {
+        return plants.get(index);
+    }
+
+    public void removePlant(Plant plant) {
+        plants.remove(plant);
+    }
+
+//    endregion
 }
