0af0fd408eec7393b1ed55957197808389fdb969
Jan Maxa <hrakuun@gmail.com>
2024-02-27 15:46:58 +0100

    # added constant for NEWFILENAME
    -constat for name of saved file

--- src/hrakuun/ja/lekce5/ukol/Settings.java
+++ src/hrakuun/ja/lekce5/ukol/Settings.java
@@ -3,12 +3,19 @@ package hrakuun.ja.lekce5.ukol;
 public class Settings {
 
     private static final String FILENAME = "data/kvetiny.txt";
+    private static final String NEWFILENAME = "data/new_kvetiny.txt";
+
     private static final String DELIMITER = "\t";
 
-    public static String getFilename(){
+    public static String getFilename() {
         return FILENAME;
     }
-    public static String getDelimiter(){
+
+    public static String getNewfilename() {
+        return NEWFILENAME;
+    }
+
+    public static String getDelimiter() {
         return DELIMITER;
     }
 }
