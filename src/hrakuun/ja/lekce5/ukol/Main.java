4015d66c920121ff65fcacb66aec9a168429aa5f
Jan Maxa <hrakuun@gmail.com>
2024-02-29 15:25:54 +0100

    # testing of sorting

--- src/hrakuun/ja/lekce5/ukol/Main.java
+++ src/hrakuun/ja/lekce5/ukol/Main.java
@@ -1,5 +1,9 @@
 package hrakuun.ja.lekce5.ukol;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -20,6 +24,21 @@ public class Main {
         } catch (PlantException e) {
             System.err.println("Nepodařilo se uložit obsah seznamu do souboru.");
         }
+        System.out.println();
+
+        List<Plant> plantsSortedByName = new ArrayList<>(listOfPlants1.getPlants());
+        Collections.sort(plantsSortedByName);
+        for (Plant plant : plantsSortedByName) {
+            System.out.println(plant.getName());
+        }
+        System.out.println();
+
+        List<Plant> plantsSortedByWatering = new ArrayList<>(listOfPlants1.getPlants());
+        plantsSortedByWatering.sort(new PlantWateringComparator());
+        for (Plant plant : plantsSortedByWatering) {
+            System.out.println(plant.getName());
+        }
+        System.out.println();
 
     }
 
