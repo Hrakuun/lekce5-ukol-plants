333eba1050fae511d915d5e9e6a78de56a453de3
Jan Maxa <hrakuun@gmail.com>
2024-02-29 15:42:15 +0100

    # added new constants for testing files with wrong inputs

--- src/hrakuun/ja/lekce5/ukol/Settings.java
+++ src/hrakuun/ja/lekce5/ukol/Settings.java
@@ -3,6 +3,8 @@ package hrakuun.ja.lekce5.ukol;
 public class Settings {
 
     private static final String FILENAME = "data/kvetiny.txt";
+    private static final String WRONGFILENAME = "data/kvetiny-spatne-datum.txt";
+    private static final String WRONGFILENAME2 = "data/kvetiny-spatne-frekvence.txt";
     private static final String NEWFILENAME = "data/new_kvetiny.txt";
 
     private static final String DELIMITER = "\t";
@@ -10,6 +12,15 @@ public class Settings {
     public static String getFilename() {
         return FILENAME;
     }
+
+    public static String getWrongFilename() {
+        return WRONGFILENAME;
+    }
+
+    public static String getWrongFilename2() {
+        return WRONGFILENAME2;
+    }
+
 
     public static String getNewfilename() {
         return NEWFILENAME;
